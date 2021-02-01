129e4b913a434b0d4716e9d9fabf3e771753815c
kosayoda <kieransiek@protonmail.com>
2021-02-01 20:46:29 +0800

    Simplify configuration.

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 
 VSCode 💡 for neovim's built-in LSP.
 
+![](https://s2.gifyu.com/images/nvim-lightbulb.gif)
+
+> Code Action selection window shown in the gif is [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim)
+
 ## Introduction/Rationale
 The plugin shows a lightbulb in the sign column whenever a `textDocument/codeAction` is available at the current cursor position.
 
 This makes code actions both [discoverable and efficient](https://rust-analyzer.github.io/blog/2020/09/28/how-to-make-a-light-bulb.html#the-mighty), as code actions can be available even when there are no visible diagnostics (warning, information, hints etc.).
 
-## Usage
+## Getting Started
 
 ### Prerequisites
 - [neovim](https://github.com/neovim/neovim) with LSP capabilities.
@@ -20,40 +24,40 @@ Example using [vim-plug](https://github.com/junegunn/vim-plug):
 Plug 'kosayoda/nvim-lightbulb'
 ```
 
-### Quickstart
-Call `require('nvim-lightbulb').update_lightbulb()` whenever you want to show a lightbulb if a code action is available at the current cursor position. Example with an [`autocmd`](https://neovim.io/doc/user/autocmd.html):
+### Usage
+Call `require('nvim-lightbulb').update_lightbulb()` whenever you want to show a lightbulb if a code action is available at the current cursor position. Example with an [`autocmd`](https://neovim.io/doc/user/autocmd.html) for all filetypes:
 
 VimScript:
 ```vim
-autocmd CursorHold * lua require'nvim-lightbulb'.update_lightbulb()
+autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()
 ```
 
 Lua:
 ```lua
-vim.cmd [[autocmd CursorHold * lua require'nvim-lightbulb'.update_lightbulb()]]
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua require'nvim-lightbulb'.update_lightbulb()]]
 ```
+
 
 ### Configuration
+
+##### Available options:
+```lua
+# Showing defaults
+require'nvim-lightbulb'.update_lightbulb {
+  sign_priority = 10
+}
+```
+
 ##### Modify the [lightbulb sign](https://neovim.io/doc/user/sign.html#:sign-define):
+
+> Fill `text`, `texthl`, `linehl`, and `numhl` according to your preferences
 
 VimScript:
 ```vim
-call sign_define("LightBulbSign", { text = "💡", texthl = "SignColumn" })
+call sign_define("LightBulbSign", { text = "", texthl = "", linehl="", numhl="" })
 ```
 
 Lua:
 ```lua
-vim.fn.sign_define("LightBulbSign", { text = "💡", texthl = "SignColumn" })
-```
-
-##### Set the [sign priority](https://neovim.io/doc/user/sign.html#sign-priority):
-
-VimScript:
-```lua
-lua require'nvim-lightbulb'.config { sign_priority = 100 }
-```
-
-Lua:
-```lua
-require'nvim-lightbulb'.config { sign_priority = 100 }
+vim.fn.sign_define("LightBulbSign", { text = "", texthl = "", linehl="", numhl="" })
 ```
--- lua/nvim-lightbulb.lua
+++ lua/nvim-lightbulb.lua
@@ -5,11 +5,6 @@ local M = {}
 local SIGN_GROUP = "nvim-lightbulb"
 local SIGN_NAME = "LightBulbSign"
 
-local _config = {
-    -- Sign priority
-    sign_priority = 10
-}
-
 -- Set default sign
 if vim.tbl_isempty(vim.fn.sign_getdefined(SIGN_NAME)) then
     vim.fn.sign_define(SIGN_NAME, { text = "💡", texthl = "LspDiagnosticsDefaultInformation" })
@@ -20,11 +15,12 @@ end
 --- Either line can be optional, and will result in just adding/removing
 --- the sign on the given line.
 ---
+--- @param priority number The priority of the sign to add
 --- @param old_line number|nil The line to remove the sign on
 --- @param new_line number|nil The line to add the sign on
 ---
 --- @private
-local function _update_sign(old_line, new_line)
+local function _update_sign(priority, old_line, new_line)
     if old_line then
         vim.fn.sign_unplace(
             SIGN_GROUP, { id = old_line, buffer = "%" }
@@ -37,7 +33,7 @@ local function _update_sign(old_line, new_line)
     if new_line then
         vim.fn.sign_place(
             new_line, SIGN_GROUP, SIGN_NAME, "%",
-            { lnum = new_line, priority = _config.sign_priority }
+            { lnum = new_line, priority = priority }
         )
         -- Update current lightbulb line
         vim.b.lightbulb_line = new_line
@@ -47,13 +43,15 @@ end
 --- Handler factory to keep track of current lightbulb line.
 ---
 --- @param line number The line when the the code action request is called
+--- @param opts table Options passed when `update_lightbulb` is called
 --- @private
-local function handler_factory(line)
+local function handler_factory(line, opts)
     --- Handler for textDocument/codeAction.
     ---
     --- See lsp-handler for more information.
     ---
     --- @private
+    local priority = opts.sign_priority or 10 -- Default sign priority
     local function code_action_handler(err, _, actions)
         -- The request returns an error
         if err then
@@ -61,7 +59,7 @@ local function handler_factory(line)
         end
         -- No available code actions
         if actions == nil or vim.tbl_isempty(actions) then
-            _update_sign(vim.b.lightbulb_line, nil)
+            _update_sign(priority, vim.b.lightbulb_line, nil)
             return
         end
 
@@ -69,29 +67,22 @@ local function handler_factory(line)
         local code_action_line = line + 1
         -- Avoid redrawing lightbulb if the code action line did not change
         if vim.b.lightbulb_line ~= code_action_line then
-            _update_sign(vim.b.lightbulb_line, code_action_line)
+            _update_sign(priority, vim.b.lightbulb_line, code_action_line)
         end
     end
 
     return code_action_handler
 end
 
-M.update_lightbulb = function()
+M.update_lightbulb = function(opts)
+    opts = opts or {}
+
     local context = { diagnostics = vim.lsp.diagnostic.get_line_diagnostics() }
     local params = lsp_util.make_range_params()
     params.context = context
     vim.lsp.buf_request(
-        0, 'textDocument/codeAction', params, handler_factory(params.range.start.line)
+        0, 'textDocument/codeAction', params, handler_factory(params.range.start.line, opts)
     )
-end
-
--- Update default config
-M.config = function(config)
-    for key, value in pairs(_config) do
-        if config[key] == nil then
-            config[key] = value
-        end
-    end
 end
 
 return M
